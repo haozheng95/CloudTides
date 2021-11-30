551ecb97d685d924dcd8b35e28c0bcf2e1871819
qijianshuai <1096324583@qq.com>
2021-11-30 12:38:48 +0800

    modify deploy

--- ui/src/environments/environment.prod.ts
+++ ui/src/environments/environment.prod.ts
@@ -3,5 +3,7 @@ import { base } from './base';
 export const environment = {
   //apiPrefix: 'http://106.14.190.68:8081/api/v1',
   apiPrefix: 'http://106.14.136.120:8081/api/v1',
+  webssh: 'http://106.14.136.120:8888',
+  apiIp: '106.14.136.120:8081',
   production: true,
 };
