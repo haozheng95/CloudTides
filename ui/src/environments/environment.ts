4e7121d69da4517c9adbe4c51eea3da23e150b4d
tesla7 <77625961@qq.com>
2021-12-16 16:40:15 +0800

    fix
    Signed-off-by: tesla7 <77625961@qq.com>

--- ui/src/environments/environment.ts
+++ ui/src/environments/environment.ts
@@ -3,14 +3,15 @@ import { base } from './base';
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-const url = window.location.href.split(':')
+const url = window.location.href.split('/')
+console.log(url)
 const http = url[0]+ ':'
 const ip = url[1]
 export const environment = {
   production: false,
-  apiPrefix: http+ip+':8033/api/v1',
+  apiPrefix: http+ip+'/api/v1',
   webssh: http+ip+':8888',
-  apiIp: ip+':8033'
+  apiIp: ip
   // apiPrefix: 'http://127.0.0.1:8033/api/v1',
   // webssh: 'http://127.0.0.1:8888',
   // apiIp: '127.0.0.1:8033'
